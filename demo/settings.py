bf32fbc160dd6a5d38ec478af8d620089fb7c1eb
chidozie <dozie.steve@gmail.com>
2021-07-22 07:51:36 +0100

    staticfiles directory added

--- demo/settings.py
+++ demo/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/usr/local/lsws/Example/html/demo/public/static'
+# STATIC_ROOT = '/usr/local/lsws/Example/html/demo/public/static'
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATICFILES_DIRS = [str(BASE_DIR / "static")]
 
 AUTHENTICATION_BACKENDS = [
    'django.contrib.auth.backends.ModelBackend',
