5491f89bde0eff6abdfe9947bbf3a61141b8414a
unknown <dozie.steve@gmail.com>
2021-09-01 19:06:23 +0100

    added transfer component

--- static/js/fundsComponent.js
+++ static/js/fundsComponent.js
@@ -9,25 +9,31 @@ let numberFormatter = new Intl.NumberFormat(Intl.locale, {
 
 })
 
+function groupBy(objectArray, property) {
+  return objectArray.reduce(function (acc, obj) {
+    let key = obj[property]
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(obj)
+    return acc
+  }, {})
+}
+
 const fundsContainer = async (funds) => {
 
     let initialCont = '';
-    let fundsdata;
-    let totalFundsSet = []
+    let mainFunds = [];
+    let totalFundsSet = [];
+    let totalFundsSet2 = [];
 
     for (const fundName in funds){
         initialCont += currencyCont(fundName, funds[fundName])
-        //total[fundName] = 0;
-        // fundsdata = funds[fundName].map(item => {
-        //
-        //     return {
-        //         [fundName]: parseFloat(item.amount) + parseFloat(item.staked)
-        //     }
-        // })
+
 
         let initialValue = 0;
         let totalFunds = await funds[fundName].reduce(async (previousValue, currentValue) => {
-            //console.log(previousValue, currentValue)
+            totalFundsSet2.push(currentValue)
             let price = await (currentValue.currency__code.includes('USD')? getCurrentPrice('EUR'+currentValue.currency__code): getCurrentPrice(currentValue.currency__code+'EUR'))
             //console.log(price)
             return await previousValue + ((parseFloat(currentValue.amount) + parseFloat(currentValue.staked)) * price.price)
@@ -38,6 +44,25 @@ const fundsContainer = async (funds) => {
             label: fundName
         })
     }
+
+    let groupedFundsSet = groupBy(totalFundsSet2, 'currency__code')
+
+    for (const currency in groupedFundsSet) {
+        let currencyTotal = await groupedFundsSet[currency].reduce((sum, currentItem) => {
+            return sum + parseFloat(currentItem.amount) + parseFloat(currentItem.staked)
+        }, 0)
+
+        let obj = {
+            total: currencyTotal,
+            currency: currency
+        };
+
+        mainFunds.push(obj)
+    }
+    //console.log(mainFunds)
+
+
+    //console.log(groupBy(totalFundsSet2, 'currency__code'))
 
     var graph = Morris.Donut({
         element: 'morris-donut-chart',
@@ -54,15 +79,16 @@ const fundsContainer = async (funds) => {
         }
     });
 
-    document.getElementById('my-funds-dom').innerHTML = initialCont
+    document.getElementById('my-funds-dom').innerHTML = initialCont;
+    document.getElementById('main-funds').innerHTML = mainComponent(mainFunds);
 }
 
 const currencyCont = (name, bal) => {
-    let initialCont = '';
-    let initialModals = '';
+    let currencyRows = '';
+    let currencyModals = '';
     let rows = bal.forEach(item => {
-        initialCont += availableCurrency(item.currency__code, item.amount, item.id)
-        initialModals += modalComponent(item.id, item.currency__code, item.amount, name)
+        currencyRows += availableCurrency(item.currency__code, item.amount, item.id, name)
+        currencyModals += item.balance_for__name==="spot" || item.balance_for__name==="main"? depositWithdrawModalComponent(item.id, item.currency__code, item.amount, name): tranferModalComponent(item.id, item.currency__code, item.amount, name)
     })
 
     return `
@@ -75,19 +101,26 @@ const currencyCont = (name, bal) => {
                 <div class="table-responsive">
                     <table class="table table-hover">
                         <tbody>
-                        ${initialCont}
+                        ${currencyRows}
     
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        ${initialModals}
+        ${currencyModals}
     </div>
     `
 }
 
-var availableCurrency = (code, amount, id) => {
+var availableCurrency = (code, amount, id, name) => {
+    let btn;
+    if (name === "main"){
+        btn = `<button data-toggle="modal" data-target="#withdrawModal${id}" type="button" class="label theme-bg2 text-white f-12">Withdraw</button>
+                <button type="button" data-toggle="modal" data-target="#depositModal${id}" class="label theme-bg text-white f-12">Deposit</button>`
+    }else {
+        btn = `<button data-toggle="modal" data-target="#transferModal${id}" type="button" class="label theme-bg2 text-white f-12">Transfer</button>`
+    }
     let currencyRow = `    
         <tr class="unread">
             <td><img class="rounded-circle" style="width:40px;"
@@ -101,15 +134,15 @@ var availableCurrency = (code, amount, id) => {
                 <h6 class="text-muted">${amount}</h6>
                 <p class="m-0 small">€ 0.00</p>
             </td>
-            <td><button data-toggle="modal" data-target="#withdrawModal${id}" type="button" class="label theme-bg2 text-white f-12">Withdraw</button><button
-                    type="button" data-toggle="modal" data-target="#depositModal${id}"
-                    class="label theme-bg text-white f-12">Deposit</button></td>
+            <td>
+                ${btn}
+            </td>
         </tr>    
     `
     return currencyRow
 }
 
-let modalComponent = (id, code, amount, name) => {
+let depositWithdrawModalComponent = (id, code, amount, name) => {
     return `<!-- Withdraw Modal -->
                                 <div class="modal fade" id="withdrawModal${id}" tabindex="-1" role="dialog" aria-labelledby="withdrawModal${id}Label" aria-hidden="true">
                                   <div class="modal-dialog" role="document">
@@ -166,5 +199,79 @@ let modalComponent = (id, code, amount, name) => {
                                   </div>
                                 </div>`
 }
+
+
+let tranferModalComponent = (id, code, amount, name) => {
+    return `<!-- tranfer Modal -->
+            <div class="modal fade" id="transferModal${id}" tabindex="-1" role="dialog" aria-labelledby="transferModal${id}Label" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="transferModal${id}Label">tranfer ${code} from ${name}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <form>
+                    <h3 class="text-center">${numberFormatter.format(amount)} ${code}</h3>
+                        <div><input type="number" class="form-control" placeholder="Amount"></div>
+                        <div>
+                            <select>
+                                <option>Spot</option>
+                                <option>Jillbot</option>
+                                <option>Jillfarm</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="customRange3">Example range</label>
+                            <input type="range" class="custom-range" min="0" max="5" step="0.5" id="customRange3">
+                        </div>                                                                                
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+`
+}
+
+
+const mainComponent = (mainFunds) => {
+
+    let currencyRows = '';
+    let currencyModals = '';
+    let rows = mainFunds.forEach(item => {
+        currencyRows += availableCurrency(item.currency, item.total, item.currency, "main")
+        currencyModals += depositWithdrawModalComponent(item.currency, item.currency, item.total, "main")
+    })
+
+    return `
+        <div class="card-block px-0 py-3">
+            <div class="card Recent-Users">
+        
+                <div class="card-header">
+                    <h5>Main</h5>
+                </div>
+                <div class="card-block px-0 py-3">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <tbody>
+                            ${currencyRows}
+        
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        ${currencyModals}
+    
+    `
+}
+
 
 fundsContainer(myFunds)
