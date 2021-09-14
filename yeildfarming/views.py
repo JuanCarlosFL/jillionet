12694a6e0c18013f97c20fb321faf56a9d8ae05a
unknown <dozie.steve@gmail.com>
2021-09-14 21:02:39 +0100

    success message for yeild order

--- yeildfarming/views.py
+++ yeildfarming/views.py
@@ -3,6 +3,8 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls.base import reverse_lazy
+from django.contrib import messages
+
 
 from .models import YeildContract, YeildOrderBook
 
@@ -16,6 +18,10 @@ class BuyContractView(LoginRequiredMixin, CreateView):
     model = YeildOrderBook
     fields = ['contract_pair']
     success_url = reverse_lazy('app:yeild')
+
+    def get_success_url(self):
+        messages.success(self.request, f'You successfully bought the <strong>{self.object.contract_pair.yeildpair}</strong> contract')
+        return super().get_success_url()
     
     def form_valid(self, form):
         form.instance.user = self.request.user
