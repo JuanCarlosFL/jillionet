7d640c1097d54852c019a5fa57106b08e727e1f6
Santiago Amigo Rabuñal <santicances@gmail.com>
2021-09-16 20:11:15 +0200

    fixed add user error

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
 from .models import UserBalance
+from orderbook.models import Currency
 
 
 User = get_user_model()
@@ -11,11 +12,15 @@ User = get_user_model()
 @receiver(post_save, sender=User)
 def create_user_balance(sender, instance, created, **kwargs):
     if created:
-        UserBalance.objects.create(currency_name="BTC", user=instance)
-        UserBalance.objects.create(currency_name="ETH", user=instance)        
-        UserBalance.objects.create(currency_name="USDT", user=instance)
-        UserBalance.objects.create(currency_name="BUSD", user=instance)
-        UserBalance.objects.create(currency_name="BNB", user=instance)
-        UserBalance.objects.create(currency_name="JILL", user=instance, ammount=5000)
+        for currency in Currency.objects.all():
+            if currency.name == 'JILL':                
+                UserBalance.objects.create(currency=currency, user=instance, ammount=5000)
+            else:
+                UserBalance.objects.create(currency=currency, user=instance)
+        #UserBalance.objects.create(currency__name="ETH", user=instance)        
+        #UserBalance.objects.create(currency__name="USDT", user=instance)
+        #UserBalance.objects.create(currency__name="BUSD", user=instance)
+        #UserBalance.objects.create(currency__name="BNB", user=instance)
+        #UserBalance.objects.create(currency__name="JILL", user=instance, ammount=5000)
     
         
